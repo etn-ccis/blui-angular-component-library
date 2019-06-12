f602514b25649110fab9e4455b667616c15a69c3
joebochill <boyle.p.joseph@gmail.com>
2019-06-12 15:54:52 -0400

    Update build test output

--- buildTest.sh
+++ buildTest.sh
@@ -4,31 +4,31 @@ GREEN='\033[0;32m'
 GRAY='\033[1;30m'
 NC='\033[0m' # No Color
 
+echo "Checking Angular Components"
 echo "Checking Root Package..."
 echo "Checking for required files..."
-echo -ne "  package.json: "
-if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 echo -ne "  readme: "
 if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 echo -ne "  license: "
 if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-
-echo "Checking Custom Component (Angular)"
-echo "Checking for required files..."
 echo -ne "  package.json: "
 if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  readme: "
-if [ ! -f .Readme.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  hero: "
+
+echo "Checking Hero..."
+echo -ne "  hero.component: "
 if [ ! -f ./core/hero/lib/hero/hero.component.d.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  hero-banner: "
+
+echo "Checking HeroBanner..."
+echo -ne "  hero-banner.component: "
 if [ ! -f ./core/hero/lib/hero-banner/hero-banner.component.d.ts  ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  channel-value: "
-if [ ! -f /core/channel-value/lib/channel-value.component.d.ts  ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+
+echo "Checking ChannelValue..."
+echo -ne "  channel-value.component: "
+if [ ! -f ./core/channel-value/lib/channel-value.component.d.ts  ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 
 
 echo -e "\r\n${GREEN}-----------------------------------"
-echo -e "angular Custom packages successfully created"
+echo -e "@pxblue/angular-components package successfully created"
 echo -e "-----------------------------------${NC}\r\n\r\n"
 
 exit 0
