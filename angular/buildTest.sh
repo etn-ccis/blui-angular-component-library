6e49618000bc05b0709c05568c09af32ab71ebcc
shivani pitta <shivani.pitta@gmail.com>
2019-06-12 13:26:12 -0400

    added test:artifacts

---
+++ angular/buildTest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
+
+echo "Checking Root Package..."
+echo "Checking for required files..."
+echo -ne "  package.json: "
+if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  readme: "
+if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  license: "
+if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+
+echo "Checking Theme (Angular)"
+echo "Checking for required files..."
+echo -ne "  package.json: "
+if [ ! -f angular/package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  readme: "
+if [ ! -f angular/Readme.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  hero: "
+if [ ! -f angular/components/projects/hero/src/lib/hero/hero.component.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  hero-banner: "
+if [ ! -f angular/components/projects/hero/src/lib/hero-banner/hero-banner.component.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  channel-value: "
+if [ ! -f angular/components/projects/channel-value/src/lib/channel-value.component.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+
+
+echo -e "\r\n${GREEN}-----------------------------------"
+echo -e "angular Custom packages successfully created"
+echo -e "-----------------------------------${NC}\r\n\r\n"
+
+exit 0
