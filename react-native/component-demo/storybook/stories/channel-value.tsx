1c49f88517aba9aad2af3e0c6c672f7f9cbd36ab
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2019-07-26 08:27:06 -0400

    Implement props and add stories for ChannelValue

--- react-native/component-demo/storybook/stories/channel-value.tsx
+++ react-native/component-demo/storybook/stories/channel-value.tsx
@@ -2,13 +2,47 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { View } from 'react-native';
 import { ChannelValue } from '@pxblue/react-native-components';
-import { text } from '@storybook/addon-knobs';
+import { text, withKnobs, boolean, select, number, color } from '@storybook/addon-knobs';
+import Leaf from '@pxblue/icons-svg/leaf.svg';
+
+const notes = {
+  notes: 'Any React Element may be passed in as `icon`; if using an svg, its color and size are not controlled by `ChannelValue`'
+};
 
 storiesOf('ChannelValue', module)
-  .add('without icon', () => (
+  .addDecorator(withKnobs)
+  .add('with only required props', () => (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <ChannelValue value={'123'} />
+      <ChannelValue
+        value={text('value', text('value', '123'))}
+      />
     </View>
-  ), {
-    notes: 'sample ChannelValue'
-  });
+  ), notes)
+  .add('with icon', () => (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <ChannelValue
+        value={text('value', text('value', '123'))}
+        icon={<Leaf width={40} height={40} fill={'#44cc44'}/>}
+      />
+    </View>
+  ), notes)
+  .add('with units', () => (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <ChannelValue
+        value={text('value', text('value', '123'))}
+        units={text('units', 'hz')}
+      />
+    </View>
+  ), notes)
+  .add('with all props', () => (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <ChannelValue
+        value={text('value', text('value', '123'))}
+        icon={<Leaf width={40} height={40} fill={'#44cc44'}/>}
+        units={text('units', 'hz')}
+        prefix={boolean('prefix', false)}
+        fontSize={number('fontSize', 20, { range: true, min: 1, max: 100, step: 1 })}
+        color={color('color', 'blue')}
+      />
+    </View>
+  ), notes);
--- react-native/components/src/channel-value.tsx
+++ react-native/components/src/channel-value.tsx
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, StyleProp, TextStyle } from 'react-native';
 
 export interface ChannelValueProps {
   value: string | number;
   icon?: React.ReactNode;
   units?: string;
-  prefix?: string;
+  prefix?: boolean;
   fontSize?: number;
   color?: string;
 }
 
 export class ChannelValue extends Component<ChannelValueProps> {
   public render() {
-    const { value, icon, units, prefix, fontSize, color } = this.props;
+    const { value, icon, fontSize, color } = this.props;
 
     return (
-      <View>
+      <View style={styles.row}>
         {icon}
-        <Text>{value}</Text>
-        <Text>{units}</Text>
+        {this.prefixUnits()}
+        <Text style={[styles.bold, { fontSize, color }]}>{value}</Text>
+        {this.suffixUnits()}
       </View>
     );
   }
+
+  private prefixUnits() {
+    const { prefix = false } = this.props;
+    if (prefix) {
+      return this.units();
+    }
+  }
+
+  private suffixUnits() {
+    const { prefix = false } = this.props;
+    if (!prefix) {
+      return this.units();
+    }
+  }
+
+  private units() {
+    const { fontSize, color, units } = this.props;
+    if (units) {
+      return (
+        <Text style={[styles.light, { fontSize, color }]}>
+          {units}
+        </Text>
+      );
+    }
+  }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  bold: {
+    fontWeight: '600'
+  },
+  light: {
+    fontWeight: '300'
+  }
+})
