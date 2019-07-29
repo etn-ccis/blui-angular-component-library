8b6231bfced3f0931d378fd64002dc06e73bec24
Patrick Pale <patrick.pale@atomicobject.com>
2019-07-29 13:42:32 -0400

    Added header and footer props to LayoutView

--- react-native/component-demo/storybook/stories/layout-view/layout-view.tsx
+++ react-native/component-demo/storybook/stories/layout-view/layout-view.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { LayoutView } from '@pxblue/react-native-components';
+import { View, Text, SafeAreaView } from 'react-native'
+
+const header = (
+  <SafeAreaView style={{flex: 0, height: '15%', backgroundColor: 'lightblue'}}>
+    <Text>I'm in a header.</Text>
+  </SafeAreaView>
+);
+
+const content = (
+  <Text style={{marginVertical: 8, marginHorizontal: 16}}>I'm in a layout view.</Text>
+);
+
+const footer = (
+  <SafeAreaView style={{flex: 0, height: '15%', backgroundColor: 'pink'}}>
+    <Text>I'm in a footer.</Text>
+  </SafeAreaView>
+);
 
 storiesOf('LayoutView', module)
-  .add('Layout View', () => (
-    <LayoutView/>
+  .add('with no header and footer', () => (
+    <LayoutView>
+      {content}
+    </LayoutView>
+  ))
+  .add('with a header and no footer', () => (
+    <LayoutView header={header}>
+      {content}
+    </LayoutView>
+  ))
+  .add('with a footer and no header', () => (
+    <LayoutView footer={footer}>
+      {content}
+    </LayoutView>
+  ))
+  .add('with a footer and header', () => (
+    <LayoutView header={header} footer={footer}>
+      {content}
+    </LayoutView>
   ));
--- react-native/components/src/layout-view/layout-view.tsx
+++ react-native/components/src/layout-view/layout-view.tsx
@@ -2,25 +2,43 @@ import React, { Fragment } from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 export interface LayoutViewProps {
-
+  header?: React.ReactElement;
+  footer?: React.ReactElement;
 }
 
 export class LayoutView extends React.Component<LayoutViewProps> {
   render() {
+    const { children } = this.props;
     return (
       <Fragment>
-        <SafeAreaView style={{backgroundColor: 'lightblue'}}/>
-        <SafeAreaView style={styles.container}>
-          <Text>I'm a layout view.</Text>
+        {this.header()}
+        <SafeAreaView style={styles.content}>
+          {children}
         </SafeAreaView>
+        {this.footer()}
       </Fragment>
     )
+  }
+
+  private header() {
+    if (this.props.header) {
+      return this.props.header;
+    } else {
+      return <SafeAreaView/>;
+    }
+  }
+
+  private footer() {
+    if (this.props.footer) {
+      return this.props.footer;
+    } else {
+      return <SafeAreaView/>
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    backgroundColor: 'lightgreen'
+  content: {
+    flex: 1
   }
 });
