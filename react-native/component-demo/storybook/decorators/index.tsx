0c387708887a69454f17915d3fca33333424b900
Patrick Pale <patrick.pale@atomicobject.com>
2019-07-30 14:49:55 -0400

    Removed unused decorator

--- react-native/component-demo/storybook/decorators/index.tsx
+++ react-native/component-demo/storybook/decorators/index.tsx
@@ -3,14 +3,6 @@ import React from 'react';
 
 export const centered = (storyFn: any) => <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>{storyFn()}</View>;
 
-export const centeredBordered = (storyFn: any) => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <View style={{borderColor: 'black', borderWidth: 1}}>
-    {storyFn()}
-    </View>
-  </View>
-);
-
 export const framedRow = (storyFn: any) =>
   <View style={{ justifyContent: 'center', height: '100%' }}>
     <View style={{ height: 2, backgroundColor: '#dddddd', flex: 1 }} />
