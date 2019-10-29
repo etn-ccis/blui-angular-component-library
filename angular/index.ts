92e5d6147c14a62831cc30f9e138e28698963d0f
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-10-29 14:19:42 -0400

    Update barrel to js and use es5 public api

--- angular/index.ts
+++
@@ -1,5 +0,0 @@
-import * as emptyState from './core/empty-state/public_api';
-import * as channelValue from './core/channel-value/public_api';
-import * as hero from './core/hero/public_api';
-
-export { channelValue, emptyState, hero };
