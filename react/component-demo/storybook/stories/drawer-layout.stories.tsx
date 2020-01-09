0566026bcb92678ca1fa99dc08042f9b96f128db
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-09 12:29:08 -0500

    Footer should not disappear when closed

--- react/component-demo/storybook/stories/drawer-layout.stories.tsx
+++ react/component-demo/storybook/stories/drawer-layout.stories.tsx
@@ -94,7 +94,7 @@ stories.add('basic usage', () => {
                }
           />
           {defaultBody}
-          {open && <DrawerFooter>
+          {<DrawerFooter>
                <div style={{'display': 'flex', 'justifyContent': 'center'}}>
                   <img src={EatonLogo} style={{'margin': '10px'}} alt="Eaton Logo" height={50} width={'auto'}/>
                </div>
