e870ee6910dd84a0c0db16f55f9f1f2f737b2d64
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-13 14:26:41 -0500

    Get package to build, using dev

--- react/component-demo/storybook/.storybook/config.js
+++ react/component-demo/storybook/.storybook/config.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import {configure, addDecorator, addParameters} from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { React as ReactThemes } from '@pxblue/themes';
+import { ReactThemes } from '@pxblue/themes';
 import * as Colors from '@pxblue/colors';
 import 'typeface-open-sans';
 import './styles.scss';
 
-addParameters({
-    options: {
-        isFullscreen: false,
-        showNav: true,
-        showPanel: true,
-        panelPosition: 'right',
-        name: 'PX Blue React Component Library'
-    }
-});
+addDecorator(storyFn => <MuiThemeProvider theme={createMuiTheme(ReactThemes.blue)}><div className={'wrapper'} style={{ color: Colors.gray['800']}}>{storyFn()}</div></MuiThemeProvider>);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.(js|tsx)$/), module);
-
-addDecorator(storyFn =>
-    <MuiThemeProvider theme={createMuiTheme(ReactThemes.blue)}>
-        <div className={'wrapper'} style={{ color: Colors.gray['800']}}>
-            {storyFn()}
-        </div>
-</MuiThemeProvider>);
