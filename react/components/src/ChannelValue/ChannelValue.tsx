d59382f4116d3b8c6218c6ccdf3cbfb08a1713b9
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-22 14:45:32 -0500

    Convert ChannelValue to FC

--- react/components/src/ChannelValue/ChannelValue.tsx
+++ react/components/src/ChannelValue/ChannelValue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { StyleRules } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import { combine } from '../utilities';
@@ -13,48 +13,38 @@ type ChannelValueProps = {
     color?: string;
 } & WithStyles;
 
-class ChannelValue extends React.Component<ChannelValueProps> {
-    public static defaultProps = {
-        prefix: false,
-        fontSize: 'inherit',
-        color: 'inherit',
-    };
+const ChannelValue: React.FC<ChannelValueProps> = (props) => {
+    const { classes, icon, value, units, prefix, fontSize, color } = props;
 
-    getUnitElement(): JSX.Element | null {
-        const { classes, units } = this.props;
+    const getUnitElement = useCallback(
+        (): JSX.Element => (
+            <>
+                {units && (
+                    <Typography variant={'h6'} color={'inherit'} className={combine([classes.text, classes.unit])}>
+                        {units}
+                    </Typography>
+                )}
+            </>
+        ),
+        [units, classes, combine]
+    );
 
-        if (!units) {
-            return null;
-        }
-
-        return (
-            <Typography variant={'h6'} color={'inherit'} className={combine([classes.text, classes.unit])}>
-                {units}
-            </Typography>
-        );
-    }
-
-    changeIconDisplay(icon: JSX.Element): JSX.Element {
-        return React.cloneElement(icon, {
-            style: Object.assign({}, icon.props.style, { display: 'block', fontSize: 'inherit' }),
+    const changeIconDisplay = (newIcon: JSX.Element): JSX.Element =>
+        React.cloneElement(newIcon, {
+            style: Object.assign({}, newIcon.props.style, { display: 'block', fontSize: 'inherit' }),
         });
-    }
 
-    render(): JSX.Element {
-        const { classes, icon, value, prefix, fontSize, color } = this.props;
-
-        return (
-            <span className={classes.wrapper} style={{ fontSize: fontSize, color: color }}>
-                {icon && <span className={classes.icon}>{this.changeIconDisplay(icon)}</span>}
-                {prefix ? this.getUnitElement() : null}
-                <Typography variant={'h6'} color={'inherit'} className={combine([classes.text, classes.value])}>
-                    {value}
-                </Typography>
-                {!prefix ? this.getUnitElement() : null}
-            </span>
-        );
-    }
-}
+    return (
+        <span className={classes.wrapper} style={{ fontSize: fontSize, color: color }}>
+            {icon && <span className={classes.icon}>{changeIconDisplay(icon)}</span>}
+            {prefix && getUnitElement()}
+            <Typography variant={'h6'} color={'inherit'} className={combine([classes.text, classes.value])}>
+                {value}
+            </Typography>
+            {!prefix && getUnitElement()}
+        </span>
+    );
+};
 
 const styles = (): StyleRules => ({
     wrapper: {
@@ -81,3 +71,8 @@ const styles = (): StyleRules => ({
 });
 
 export default withStyles(styles)(ChannelValue);
+ChannelValue.defaultProps = {
+    prefix: false,
+    fontSize: 'inherit',
+    color: 'inherit',
+};
