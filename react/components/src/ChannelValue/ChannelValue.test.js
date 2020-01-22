2b27a1e9801729064485f7ed1d598a0aabb506f7
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-22 10:17:06 -0500

    Refactor unit test

--- react/components/src/ChannelValue/ChannelValue.test.js
+++ react/components/src/ChannelValue/ChannelValue.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import ChannelValue from './ChannelValue';
-import Enzyme from 'enzyme';
+import * as Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Menu from '@material-ui/icons/Menu';
 import {
     createMount,
     createShallow,
-    createRender,
-    // describeConformance,
     getClasses,
 } from '@material-ui/core/test-utils';
 
@@ -16,7 +14,6 @@ import {
 Enzyme.configure({adapter: new Adapter()});
 let mount;
 let shallow;
-let render;
 let classes;
 
 describe("ChannelValue", () => {
@@ -24,7 +21,6 @@ describe("ChannelValue", () => {
     beforeEach(() => {
         mount = createMount({ strict: true });
         shallow = createShallow({ dive: true });
-        render = createRender();
         classes = getClasses(<ChannelValue value={0} units={'test'} icon={<Menu/>} prefix />);
     });
 
@@ -45,19 +41,19 @@ describe("ChannelValue", () => {
     });
     it('should render value properly', () => {
         const wrapper = shallow(<ChannelValue value={1}/>);
-        expect(wrapper.find('.'+classes.value).length).toEqual(1);
+        expect(wrapper.find(`.${classes.value}`).length).toEqual(1);
     });
     it('should render icon properly', () => {
         let wrapper = shallow(<ChannelValue icon={<Menu/>} value={1}/>);
-        expect(wrapper.find('.'+classes.icon).length).toEqual(1);
+        expect(wrapper.find(`.${classes.icon}`).length).toEqual(1);
         wrapper = shallow(<ChannelValue value={1}/>);
-        expect(wrapper.find('.'+classes.icon).length).toEqual(0);
+        expect(wrapper.find(`.${classes.icon}`).length).toEqual(0);
     });
     it('should render units properly', () => {
         let wrapper = shallow(<ChannelValue value={1} units={'X'}/>);
-        expect(wrapper.find('.'+classes.unit).length).toEqual(1);
+        expect(wrapper.find(`.${classes.unit}`).length).toEqual(1);
         wrapper = shallow(<ChannelValue value={1}/>);
-        expect(wrapper.find('.'+classes.unit).length).toEqual(0);
+        expect(wrapper.find(`.${classes.unit}`).length).toEqual(0);
     });
 
 });
