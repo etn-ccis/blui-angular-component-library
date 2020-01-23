7148edfaf05a8a135015942b9ceaded035399f31
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-23 12:16:37 -0500

    Convert HeroBanner tests to TS

--- react/components/src/HeroBanner/HeroBanner.test.tsx
+++ react/components/src/HeroBanner/HeroBanner.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HeroBanner from './HeroBanner';
 import Hero from '../Hero';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
