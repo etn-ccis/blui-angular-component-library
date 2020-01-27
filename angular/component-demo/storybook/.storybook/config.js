5c790b06936d0f63cc511d6c51649ad08e9ac22f
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-27 09:10:28 -0500

    Create empty-state story, wip

--- angular/component-demo/storybook/.storybook/config.js
+++ angular/component-demo/storybook/.storybook/config.js
@@ -1,4 +1,38 @@
-import { configure } from '@storybook/angular';
+import {addParameters, configure} from '@storybook/angular';
+import pxblue from './pxblue-theme';
+
+const newViewports = {
+    iPhone5: {
+        name: 'iPhone 5',
+        styles: {
+            width: '320px',
+            height: '568px',
+        },
+    },
+    iPhone6: {
+        name: 'iPhone 6',
+        styles: {
+            width: '375px',
+            height: '667px',
+        },
+    },
+    iPad: {
+        name: 'iPad',
+        styles: {
+            width: '768px',
+            height: '1024px',
+        },
+    },
+};
+
+addParameters({
+    viewport: {
+        viewports: newViewports
+    },
+    options: {
+        theme: pxblue,
+    },
+});
 
 // automatically import all files ending in *.stories.ts
 configure(require.context('../src/stories', true, /\.stories\.ts$/), module);
