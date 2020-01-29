f5ab1cc5309f730aee583b4d9d440196506a9072
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-28 21:54:13 -0500

    Try to reduce top-level configuration files

--- components/.eslintrc.js
+++ components/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports =  {
   parser:  '@typescript-eslint/parser',
   extends:  [ '@pxblue/eslint-config/ts' ],
   parserOptions:  {
-    project: "./tsconfig.json",
+    project: "./config/tsconfig.json",
   },
   env: {
     browser: true
