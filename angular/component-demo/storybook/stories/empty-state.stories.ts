14ec918db898a5be48b790546512a0d9df49d32d
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-29 11:23:09 -0500

    Add another example to empty state story

--- angular/component-demo/storybook/stories/empty-state.stories.ts
+++ angular/component-demo/storybook/stories/empty-state.stories.ts
@@ -1,3 +1,5 @@
+import {MatButtonModule} from "@angular/material/button";
+import {MatRippleModule} from "@angular/material/core";
 import {MatIconModule} from "@angular/material/icon";
 import {EmptyStateModule} from '@pxblue/angular-components';
 import {text, withKnobs} from "@storybook/addon-knobs";
@@ -9,20 +11,34 @@ storiesOf('Components|Empty State', module)
       moduleMetadata({
          imports: [
             EmptyStateModule,
-            MatIconModule
+            MatButtonModule,
+            MatIconModule,
+            MatRippleModule,
          ],
       })
    )
    .addDecorator(withKnobs)
    .addDecorator(wrap())
-   .add('text only', () => ({
+   .add('as text only', () => ({
       template: `
           <pxb-empty-state [title]="title">
             <!--<mat-icon>devices</mat-icon>-->
           </pxb-empty-state>
       `,
       props: {
-         title: text('title', 'Just Text')
+         title: text('title', 'No Alarms Found')
+      }
+   }))
+   .add('as a placeholder', () => ({
+      template: `
+          <pxb-empty-state [title]="title" [description]="description">
+            <div empty-icon>(icon)</div>
+            <button actions mat-stroked-button color="primary">Primary</button>
+          </pxb-empty-state>
+      `,
+      props: {
+         title: text('title', 'Predictions Page Coming Soon'),
+         description: text('description', 'A fully redesigned predictions page is coming in our next release!')
       }
    })
 );
