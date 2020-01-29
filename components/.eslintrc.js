289fdd41a1fd6b84b7c2bedef5b8d70dfc2a9414
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-29 14:22:10 -0500

    Revert config folder changes

--- components/.eslintrc.js
+++ components/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports =  {
   parser:  '@typescript-eslint/parser',
   extends:  [ '@pxblue/eslint-config/ts' ],
   parserOptions:  {
-    project: "./config/tsconfig.json",
+    project: "./tsconfig.json",
   },
   env: {
     browser: true
