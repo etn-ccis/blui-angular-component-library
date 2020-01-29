ccd96bcb19e500fc4eb7e3b928181168c94c3f3c
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-29 09:56:07 -0500

    Add pxblue theme POC to empty-state story

--- angular/component-demo/storybook/.storybook/webpack.config.js
+++ angular/component-demo/storybook/.storybook/webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = ({ config }) => {
     config.module.rules.push({
         test: /\.(ts)$/,
@@ -7,6 +8,18 @@ module.exports = ({ config }) => {
             },
         ],
     });
+    config.module.rules.push({
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    });
+    config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css' }))
     config.watchOptions = { ignored: [ /node_modules([\\]+|\/)+(?!@pxblue)/ ] };
     config.resolve.extensions.push('.ts');
     return config;
