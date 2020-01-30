298a8a57308cb9610c882c968690412ac461bd17
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-29 19:18:50 -0500

    Add css loaders to webpack to fix prod build

--- react/demos/storybook/.storybook/webpack.config.js
+++ react/demos/storybook/.storybook/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = ({ config }) => {
     config.module.rules.push({
@@ -9,6 +10,18 @@ module.exports = ({ config }) => {
             },
         ],
     });
+    config.module.rules.push({
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    });
+    config.plugins.push(new MiniCssExtractPlugin({ filename: '[name].css' }))
     config.watchOptions = { ignored: [ /node_modules([\\]+|\/)+(?!@pxblue)/ ] };
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
