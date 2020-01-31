21acf60266fbf9bb36ee55cdf3ede308c52c50be
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-01-31 13:01:28 -0500

    Match react folder structure

--- angular/components/src/index.ts
+++ angular/components/src/index.ts
@@ -1,4 +1,1 @@
-export * from './channel-value/public-api';
-export * from './empty-state/public-api';
-export * from './hero/public-api';
-export * from './hero-banner/public-api';
+export * from './core';
