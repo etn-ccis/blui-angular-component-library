1db084c4fb4def0117fa799874f93cc9b73618fe
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-01-31 14:39:45 -0500

    Fix the react build, add a buildComponents script

--- react/scripts/buildComponents.sh
+++ react/scripts/buildComponents.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Script to be ran before publishing a new version of the package.
-# Copies resources into the dist folder.
 BLUE='\033[0;34m'
 BBLUE='\033[1;34m' #BOLD
 PURPLE='\033[0;35m'
@@ -11,12 +9,17 @@ BGREEN='\033[1;32m' #BOLD
 GRAY='\033[1;30m'
 NC='\033[0m' # No Color
 
+# Remove previous build
+rm -rf ./dist
+
+cd ./components
+yarn && yarn build
 cd ..
+
 echo -e "${BLUE}Copying Package Resources${NC}"
 cp -r package.json ./dist/package.json
 cp -r README.md ./dist/README.md
 cp -r LICENSE ./dist/LICENSE
 cp -r CHANGELOG.md ./dist/CHANGELOG.md
-
 
 echo -e "${GRAY}Complete${NC}\r\n"
