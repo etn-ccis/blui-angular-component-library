ba2b18e4967cb05a0197dcc2cedb636c8770741f
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-02-06 10:30:52 -0500

    Make custom body example more interesting

--- react/demos/storybook/stories/user-menu.stories.tsx
+++ react/demos/storybook/stories/user-menu.stories.tsx
@@ -1,12 +1,13 @@
-import {MenuItem} from '@material-ui/core';
-import {Email, Settings} from '@material-ui/icons';
+import { Divider, MenuItem, Typography } from '@material-ui/core';
+import { Email, Settings } from '@material-ui/icons';
 import SendIcon from '@material-ui/icons/Send';
 import * as Colors from '@pxblue/colors';
-import {UserMenu, UserMenuGroup} from '@pxblue/react-components';
-import {action} from "@storybook/addon-actions";
-import {color, number, text, withKnobs} from '@storybook/addon-knobs';
-import {storiesOf} from '@storybook/react';
+import { UserMenu, UserMenuGroup } from '@pxblue/react-components';
+import { action } from '@storybook/addon-actions';
+import { color, number, text, withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
 import React from 'react';
+const EatonLogo = require('../assets/EatonLogo.svg');
 
 const tRex = require('../assets/trex.jpeg');
 
@@ -15,7 +16,6 @@ stories.addDecorator(withKnobs);
 stories.addParameters({
     notes: { markdown: require('./../../../docs/UserMenu.md') },
 });
-
 
 /*
 const useStyles = makeStyles((theme: Theme) =>
@@ -34,17 +34,17 @@ const defaultMenuItems: UserMenuGroup[] = [
             {
                 title: 'Log Out',
                 icon: <SendIcon />,
-                onClick: action('Log Out Selected')
+                onClick: action('Log Out Selected'),
             },
             {
                 title: 'Account Settings',
                 icon: <Settings />,
-                onClick: action('Account Settings Selected')
+                onClick: action('Account Settings Selected'),
             },
             {
                 title: 'Contact Us',
                 icon: <Email />,
-                onClick: action('Contact Us Selected')
+                onClick: action('Contact Us Selected'),
             },
         ],
     },
@@ -79,7 +79,7 @@ stories.add('with menu header', () => {
     const menuSubtitle = text('menuSubtitle', 'Menu Subtitle');
     return (
         <UserMenu
-           width={width}
+            width={width}
             menuGroups={defaultMenuItems}
             menuTitle={menuTitle}
             menuSubtitle={menuSubtitle}
@@ -91,9 +91,29 @@ stories.add('with menu header', () => {
 stories.add('with custom menu body', () => {
     return (
         <UserMenu avatarProps={{ src: tRex }}>
+            <div style={{ position: 'relative', padding: 10, textAlign: 'center' }}>
+                <Typography variant={'h5'}>Welcome, Trex</Typography>
+                <div
+                    style={{
+                        position: 'absolute',
+                        left: 0,
+                        top: 0,
+                        height: 50,
+                        width: '100%',
+                        opacity: 0.1,
+                        backgroundSize: 'contain',
+                        backgroundImage: `url(${tRex})`,
+                    }}
+                >
+                    &nbsp;
+                </div>
+            </div>
+            <Divider />
             <MenuItem key={'profile'}>Profile</MenuItem>
             <MenuItem key={'account'}>My account</MenuItem>
             <MenuItem key={'logout'}>Logout</MenuItem>
+            <Divider />
+            <img style={{ textAlign: 'center', padding: 20, paddingBottom: 0 }} src={EatonLogo} />
         </UserMenu>
     );
 });
