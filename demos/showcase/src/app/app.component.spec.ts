b844e55a069f061a4fcde9d8811e7e8fa15bb9e0
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-02-26 10:51:19 -0500

    Run prettier

--- demos/showcase/src/app/app.component.spec.ts
+++ demos/showcase/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroModule, ChannelValueModule } from '@pxblue/angular-components';
@@ -8,47 +8,41 @@ import { HeroModule, ChannelValueModule } from '@pxblue/angular-components';
 //pxblue modules
 import { NgProgressIconsModule } from '@pxblue/ng-progress-icons';
 
-
-import {MatDividerModule} from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-
-
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-       AppComponent
-      ],
-      imports: [
-        BrowserModule,
-        HeroModule,
-        ChannelValueModule,
-        FormsModule,
-        MatDividerModule,
-        MatIconModule,
-        MatCardModule,
-        MatListModule,
-        HttpClientModule,
-        //pxblue
-        NgProgressIconsModule,
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [
+                BrowserModule,
+                HeroModule,
+                ChannelValueModule,
+                FormsModule,
+                MatDividerModule,
+                MatIconModule,
+                MatCardModule,
+                MatListModule,
+                HttpClientModule,
+                //pxblue
+                NgProgressIconsModule,
+            ],
+        }).compileComponents();
+    }));
 
-      ],
-    }).compileComponents();
-  }));
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  // it(`should have as title 'Hero-Component'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('Hero-Component');
-  // });
+    // it(`should have as title 'Hero-Component'`, () => {
+    //   const fixture = TestBed.createComponent(AppComponent);
+    //   const app = fixture.debugElement.componentInstance;
+    //   expect(app.title).toEqual('Hero-Component');
+    // });
 });
--- demos/showcase/src/app/app.component.ts
+++ demos/showcase/src/app/app.component.ts
@@ -1,27 +1,27 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from "@angular/material/icon";
-import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import * as PXBColors from '@pxblue/colors';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const iconSet = require("@pxblue/icons-svg/icons.svg");
+const iconSet = require('@pxblue/icons-svg/icons.svg');
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  colors: Record<string, any>;
-  constructor(
-    private readonly matIconRegistry: MatIconRegistry,
-    private readonly domSanitizer: DomSanitizer
-  ) {
-    this.colors = PXBColors;
-    this.matIconRegistry.addSvgIconSetInNamespace('px-icons', this.domSanitizer.bypassSecurityTrustResourceUrl(iconSet));
-  }
+    colors: Record<string, any>;
+    constructor(private readonly matIconRegistry: MatIconRegistry, private readonly domSanitizer: DomSanitizer) {
+        this.colors = PXBColors;
+        this.matIconRegistry.addSvgIconSetInNamespace(
+            'px-icons',
+            this.domSanitizer.bypassSecurityTrustResourceUrl(iconSet)
+        );
+    }
 
-  test(): void {
-    // eslint-disable-next-line no-alert
-    alert('Hero component');
-  }
+    test(): void {
+        // eslint-disable-next-line no-alert
+        alert('Hero component');
+    }
 }
