e6976820abc3c3febfa335891c2239b7e441f804
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-04-02 13:04:33 -0400

    Fix test util to make sense

--- components/src/utils/test-utils.ts
+++ components/src/utils/test-utils.ts
@@ -2,6 +2,6 @@ import { ComponentFixture } from '@angular/core/testing';
 
 // Using a selector, counts the number of instances.
 export const count = (fixture: ComponentFixture<any>, selector: string, expected = 1): void => {
-    const found = fixture.nativeElement.querySelectorAll(selector);
-    expect(found.length).toBe(expected, `Expected ${expected} instances of '${selector}', but found ${count.length}`);
+    const instances = fixture.nativeElement.querySelectorAll(selector);
+    expect(instances.length).toBe(expected, `Expected ${expected} instances of '${selector}', but found ${instances.length}`);
 };
