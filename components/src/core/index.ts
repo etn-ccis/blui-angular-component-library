9bf990d84f4f0c8043363075ff6bdd079bac396b
andrupu <47467951+andrupu@users.noreply.github.com>
2020-04-03 15:40:49 -0400

    import PXBlue icons and update html, module.ts, index.ts

--- components/src/core/index.ts
+++ components/src/core/index.ts
@@ -1,3 +1,4 @@
 export * from './channel-value/public-api';
 export * from './empty-state/public-api';
 export * from './hero/public-api';
+export * from './info-list-item/public-api';
--- components/src/core/info-list-item/info-list-item.module.ts
+++ components/src/core/info-list-item/info-list-item.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoListItemComponent } from './info-list-item.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [InfoListItemComponent],
-  imports: [CommonModule],
+  imports: [
+    NgModule,
+    CommonModule,
+    MatIconModule,
+    HttpClientModule
+  ],
   exports: [InfoListItemComponent]
 })
 export class InfoListItemModule { }
