cd4a378f0d14c6e941e97fa0e9cdf66c945b1ace
andrupu <47467951+andrupu@users.noreply.github.com>
2020-04-08 16:27:51 -0400

    Initialize component and add module and public api

---
+++ components/src/core/list-item-tag/list-item-tag.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListItemTagComponent } from './list-item-tag.component';
+
+@NgModule({
+    declarations: [ListItemTagComponent],
+    imports: [CommonModule],
+    exports: [ListItemTagComponent],
+})
+export class ListItemTagModule {}
