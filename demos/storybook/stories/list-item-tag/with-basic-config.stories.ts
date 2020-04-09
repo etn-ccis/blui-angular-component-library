3c67f39e4328a42c0d10eeec85660083af9992d8
andrupu <47467951+andrupu@users.noreply.github.com>
2020-04-09 16:23:39 -0400

    Add stories.ts files and update local dependencies

---
+++ demos/storybook/stories/list-item-tag/with-basic-config.stories.ts
@@ -0,0 +1,12 @@
+import { text } from '@storybook/addon-knobs';
+
+export const withBasicConfig = (): any => ({
+    template: `
+          <pxb-list-item-tag [label]="label">
+            <h2> {{label}} </h2>
+          </pxb-list-item-tag>
+      `,
+    props: {
+        label: text('label'),
+    },
+});
