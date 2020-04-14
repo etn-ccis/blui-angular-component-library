aa87d50e87c2a5f6c004e7481397998805fedff5
daileytj <daileytj89@gmail.com>
2020-04-14 08:16:58 -0400

    load light theme on initial page load

--- demos/storybook/.storybook/config.js
+++ demos/storybook/.storybook/config.js
@@ -30,6 +30,9 @@ pxblueTheme.brandTitle = 'PX Blue Angular Component Library';
 pxblueTheme.brandImage =  require('../assets/pxblue-angular.svg');
 pxblueTheme.brandUrl = 'https://pxblue.github.io';
 
+const themeInit = { dark: pxblueTheme, light: pxblueTheme, current: 'light' }
+window.localStorage.setItem('sb-addon-themes-3', JSON.stringify(themeInit));
+
 addParameters({
     /* Users will see this while the component is loading. */
     notes: {
