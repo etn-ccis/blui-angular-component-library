7368426e95c4558f0f5a38d8e0cb2ffce739bf82
andrupu <47467951+andrupu@users.noreply.github.com>
2020-04-23 15:24:43 -0400

    Add theming to withBasicConfig story and fontColor to withinAnInfoListItem story

--- demos/storybook/stories/list-item-tag/with-basic-config.stories.ts
+++ demos/storybook/stories/list-item-tag/with-basic-config.stories.ts
@@ -1,10 +1,18 @@
 import { text } from '@storybook/addon-knobs';
+// import * from '@pxblue/angular-themes';
+import * as Colors from '@pxblue/colors';
+import { WITH_MIN_PROPS_STORY_NAME } from '../../src/constants';
 
 export const withBasicConfig = (): any => ({
+
+    name: WITH_MIN_PROPS_STORY_NAME,
+
     template: `
           <pxb-list-item-tag [label]="label"></pxb-list-item-tag>
       `,
     props: {
         label: text('label', 'active'),
+        colors: Colors,
     },
+
 });
