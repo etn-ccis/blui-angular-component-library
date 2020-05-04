859a60b3b7aa958c6f9b199e5eda1143eef971fe
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-05-04 14:23:41 -0400

    Match React for context story

--- demos/storybook/stories/list-item-tag/_module.stories.ts
+++ demos/storybook/stories/list-item-tag/_module.stories.ts
@@ -9,10 +9,24 @@ import {
     WITH_FULL_CONFIG_STORY_NAME,
     WITH_MIN_PROPS_STORY_NAME,
 } from '../../src/constants';
-import { getReadMe, getReadMeStory, storyWrapper, UtilModule } from '../../src/utils';
+import {getReadMe, getReadMeStory, storyWrapper, UtilModule} from '../../src/utils';
 import { withBasicConfig } from './with-basic-config.stories';
 import { withFullConfig } from './with-full-config.stories';
 import { withinAnInfoListItem } from './within-an-InfoListItem.stories';
+import {infoListItemWrapper} from "../info-list-item/_module.stories";
+
+storiesOf(`${COMPONENT_SECTION_NAME}/List Item Tag`, module)
+    .addDecorator(
+        moduleMetadata({
+            imports: [ListItemTagModule, MatIconModule, UtilModule],
+        })
+    )
+    .addDecorator(withKnobs)
+    .addDecorator(storyWrapper())
+    .addParameters({ ...STORY_PARAMS, notes: { markdown: getReadMe('ListItemTag.md') } })
+    .add(README_STORY_NAME, getReadMeStory)
+    .add(WITH_MIN_PROPS_STORY_NAME, withBasicConfig)
+    .add(WITH_FULL_CONFIG_STORY_NAME, withFullConfig);
 
 storiesOf(`${COMPONENT_SECTION_NAME}/List Item Tag`, module)
     .addDecorator(
@@ -22,8 +36,6 @@ storiesOf(`${COMPONENT_SECTION_NAME}/List Item Tag`, module)
     )
     .addDecorator(withKnobs)
     .addDecorator(storyWrapper())
+    .addDecorator(infoListItemWrapper())
     .addParameters({ ...STORY_PARAMS, notes: { markdown: getReadMe('ListItemTag.md') } })
-    .add(README_STORY_NAME, getReadMeStory)
-    .add(WITH_MIN_PROPS_STORY_NAME, withBasicConfig)
-    .add(WITH_FULL_CONFIG_STORY_NAME, withFullConfig)
     .add('within an InfoListItem', withinAnInfoListItem);
--- demos/storybook/stories/list-item-tag/within-an-InfoListItem.stories.ts
+++ demos/storybook/stories/list-item-tag/within-an-InfoListItem.stories.ts
@@ -1,21 +1,18 @@
 import * as Colors from '@pxblue/colors';
-// import { InfoListItem, ListItemTag } from '@pxblue/angular-components';
 
 export const withinAnInfoListItem = (): any => ({
     template: `
-        <div style="background-color: white;">
-            <pxb-info-list-item style="display: flex; justify-content: center; width: 80%;">
-                <div title>Info List Item </div>
-                <div subtitle>with a ListItemTag component to the right</div>
-                <mat-icon [style.color]="colors.blue[500]" icon>battery_charging_full</mat-icon>
-                <div rightContent style="width: 180px; display: flex; justify-content: space-between; align: right;">
-                    <pxb-list-item-tag label="Build Passing" [backgroundColor]="colors.green[500]" [fontColor]="colors.white[50]"></pxb-list-item-tag>
-                    <pxb-list-item-tag label="5 Bugs" [backgroundColor]="colors.yellow[500]" [fontColor]="colors.white[50]"></pxb-list-item-tag>
-                </div>
-            </pxb-info-list-item>
-        </div>
-      `,
+        <pxb-info-list-item>
+            <div title>Info List Item </div>
+            <div subtitle>with a ListItemTag component to the right</div>
+            <mat-icon [style.color]="colors.blue[500]" icon>battery_charging_full</mat-icon>
+            <div rightContent style="width: 180px; display: flex; justify-content: space-between;">
+                <pxb-list-item-tag label="Build Passing" [backgroundColor]="colors.green[500]" [fontColor]="colors.white[50]"></pxb-list-item-tag>
+                <pxb-list-item-tag label="5 Bugs" [backgroundColor]="colors.yellow[500]" [fontColor]="colors.white[50]"></pxb-list-item-tag>
+            </div>
+        </pxb-info-list-item>
+    `,
     props: {
         colors: Colors
-    }
+    },
 });
