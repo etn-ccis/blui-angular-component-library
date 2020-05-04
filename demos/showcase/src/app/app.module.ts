a834bd8d0c62306a13bb7f1060d6c54380c005f2
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-05-04 13:41:38 -0400

    Add to showcase demo

--- demos/showcase/src/app/app.module.ts
+++ demos/showcase/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
     DrawerHeaderModule,
     DrawerSubheaderModule,
     DrawerFooterModule,
+    ListItemTagModule
 } from '@pxblue/angular-components';
 
 //material modules
@@ -46,6 +47,7 @@ import { MatButtonModule } from '@angular/material/button';
         DrawerHeaderModule,
         DrawerSubheaderModule,
         DrawerFooterModule,
+        ListItemTagModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
