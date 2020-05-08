c8fa38ab5a7e7aa5fc5e775cd89092c8b8e0486e
joebochill <boyle.p.joseph@gmail.com>
2020-05-08 10:49:21 -0400

    Fix text alignment in welcomse story button

--- demos/storybook/src/welcome/welcome.component.ts
+++ demos/storybook/src/welcome/welcome.component.ts
@@ -29,19 +29,13 @@ import { CommonModule } from '@angular/common';
                     <div class="mat-h3 description">
                         Learn about and interact with our PX Blue components using Storybook.
                     </div>
-                    <div class="buttons">
-                        <a target="_blank" href="https://github.com/pxblue/angular-component-library" mat-stroked-button
-                            [style.color]="pxbColors.white[50]"
-                            [style.borderColor]="pxbColors.white[50]"
-                            class="link">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="github" [style.fill]="pxbColors.white[50]">
-                                <title>github</title>
-                                <rect width="24" height="24" fill="none"/>
-                                <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z"/>
-                            </svg>
-                            <span class="mat-h4">Github</span>
-                        </a>
-                    </div>
+                    <a mat-stroked-button actions href="https://github.com/pxblue/angular-component-library" target="blank" [style.marginTop.px]="36">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="github" [style.fill]="pxbColors.white[50]">
+                            <rect width="24" height="24" fill="none"/>
+                            <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z"/>
+                        </svg>
+                        Github
+                    </a>
                 </section>
             </div>
         </div>
@@ -66,4 +60,4 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     declarations: [WelcomeComponent],
     exports: [WelcomeComponent, UtilModule],
 })
-export class WelcomeModule {}
+export class WelcomeModule { }
