f2afa2b3ff5d668ed1feb8e455e842ce4f84e26d
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-05-15 07:38:24 -0400

    No barrel imports allowed in ng v9

--- components/src/core/drawer/drawer-header/drawer-header.module.ts
+++ components/src/core/drawer/drawer-header/drawer-header.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DrawerHeaderComponent } from './drawer-header.component';
-import { MatToolbarModule, MatIconModule, MatDividerModule } from '@angular/material';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
     declarations: [DrawerHeaderComponent],
