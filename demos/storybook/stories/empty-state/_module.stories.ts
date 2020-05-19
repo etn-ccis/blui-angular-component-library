21fef5335c0e8dcd396c98c531bc264641b978cb
huayunh <hhuayun0828@gmail.com>
2020-05-19 12:35:30 -0400

    Update white background

--- demos/storybook/stories/empty-state/_module.stories.ts
+++ demos/storybook/stories/empty-state/_module.stories.ts
@@ -10,12 +10,26 @@ import {
     WITH_FULL_CONFIG_STORY_NAME,
     WITH_MIN_PROPS_STORY_NAME,
 } from '../../src/constants';
-import { getReadMe, getReadMeStory, storyWrapper, UtilModule } from '../../src/utils';
+import { getReadMe, getReadMeStory, storyWrapper, UtilModule, isDarkMode } from '../../src/utils';
 import { withDescription } from './with-description.stories';
 import { withBasicConfig } from './basic-config.stories';
 import { withActions } from './with-actions.stories';
 import { withFullConfig } from './with-full-config.stories';
 import { withA11y } from '@storybook/addon-a11y';
+import * as Colors from '@pxblue/colors';
+
+export const useWhiteBackground = () => (storyFn): any => {
+    const story = storyFn();
+    return {
+        ...story,
+        template: `<div [style.backgroundColor]="getDecoratorBgColor()" id="white-background-story-wrapper">${story.template}</div>`,
+        styles: [`#white-background-story-wrapper {width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center;}`],
+        props: {
+            ...story.props,
+            getDecoratorBgColor: (): string => isDarkMode() ? Colors.darkBlack[900] : 'white',
+        },
+    };
+}
 
 storiesOf(`${COMPONENT_SECTION_NAME}/Empty State`, module)
     .addDecorator(
@@ -27,6 +41,7 @@ storiesOf(`${COMPONENT_SECTION_NAME}/Empty State`, module)
     // @accessibility
     .addDecorator(withA11y)
     .addDecorator(storyWrapper())
+    .addDecorator(useWhiteBackground())
     .addParameters({ ...STORY_PARAMS, notes: { markdown: getReadMe('EmptyState.md') } })
     .add(README_STORY_NAME, getReadMeStory)
     .add(WITH_MIN_PROPS_STORY_NAME, withBasicConfig)
--- demos/storybook/stories/info-list-item/_module.stories.ts
+++ demos/storybook/stories/info-list-item/_module.stories.ts
@@ -1,23 +1,23 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { moduleMetadata, storiesOf } from '@storybook/angular';
-import {InfoListItemModule, ChannelValueModule} from "@pxblue/angular-components";
+import { InfoListItemModule, ChannelValueModule } from "@pxblue/angular-components";
 import {
     COMPONENT_SECTION_NAME,
     README_STORY_NAME,
     STORY_PARAMS, WITH_FULL_CONFIG_STORY_NAME,
     WITH_MIN_PROPS_STORY_NAME,
 } from '../../src/constants';
-import {getReadMe, getReadMeStory, isDarkMode, storyWrapper, UtilModule} from '../../src/utils';
+import { getReadMe, getReadMeStory, isDarkMode, storyWrapper, UtilModule } from '../../src/utils';
 import { withBasicConfig } from './with-basic-config.stories';
 import { withIcon } from "./with-icon.stories";
 import { withSubtitle } from "./with-subtitle.stories";
-import {withStatus} from "./with-status.stories";
-import {withFullConfig} from "./with-full-config.stories";
-import {MatRippleModule} from "@angular/material/core";
-import {withinList} from "./within-list.stories";
+import { withStatus } from "./with-status.stories";
+import { withFullConfig } from "./with-full-config.stories";
+import { MatRippleModule } from "@angular/material/core";
+import { withinList } from "./within-list.stories";
 import { withA11y } from '@storybook/addon-a11y';
-import {withRightContent} from "./with-right-content.stories";
-import {withLeftContent} from "./with-left-content.stories";
+import { withRightContent } from "./with-right-content.stories";
+import { withLeftContent } from "./with-left-content.stories";
 import * as Colors from '@pxblue/colors';
 
 export const infoListItemWrapper = () => (storyFn): any => {
@@ -25,7 +25,7 @@ export const infoListItemWrapper = () => (storyFn): any => {
     return {
         ...story,
         template: `<div style="width: 90%" [style.backgroundColor]="getDecoratorBgColor()">${story.template}</div>`,
-        styles: [`:host { display: flex; width: 100%; justify-content: center;`],
+        styles: [`:host { display: flex; width: 100%; justify-content: center;}`],
         props: {
             ...story.props,
             getDecoratorBgColor: (): string => isDarkMode() ? Colors.black[500] : 'white',
