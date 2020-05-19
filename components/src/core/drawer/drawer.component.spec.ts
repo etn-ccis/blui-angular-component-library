da19ce6540a6308c943012f1f12649e7148b386e
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-05-19 10:09:04 -0400

    Drawer Test

--- components/src/core/drawer/drawer.component.spec.ts
+++ components/src/core/drawer/drawer.component.spec.ts
@@ -1,14 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {count} from "../../utils/test-utils";
 import { DrawerComponent } from './drawer.component';
 import { DrawerModule } from './drawer.module';
 import { Component } from '@angular/core';
 
 @Component({
     template: `
-        <pxb-drawer> </pxb-drawer>
+        <pxb-drawer>
+            <pxb-drawer-header id="test-header"></pxb-drawer-header>
+        </pxb-drawer>
     `,
 })
-class TestDrawer {}
+class DrawerHeaderRenderTest {}
+
+@Component({
+    template: `
+        <pxb-drawer>
+            <pxb-drawer-subheader id="test-subheader"></pxb-drawer-subheader>
+        </pxb-drawer>
+    `,
+})
+class DrawerSubheaderRenderTest {}
+
+@Component({
+    template: `
+        <pxb-drawer>
+            <pxb-drawer-body id="test-body"></pxb-drawer-body>
+        </pxb-drawer>
+    `,
+})
+class DrawerBodyRenderTest {}
+
+@Component({
+    template: `
+        <pxb-drawer>
+            <pxb-drawer-footer id="test-footer"></pxb-drawer-footer>
+        </pxb-drawer>
+    `,
+})
+class DrawerFooterRenderTest {}
+
 
 describe('DrawerComponent', () => {
     let component: DrawerComponent;
@@ -16,15 +47,49 @@ describe('DrawerComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [TestDrawer],
+            declarations: [DrawerHeaderRenderTest, DrawerSubheaderRenderTest, DrawerBodyRenderTest, DrawerFooterRenderTest],
             imports: [DrawerModule],
         }).compileComponents();
         fixture = TestBed.createComponent(DrawerComponent);
         component = fixture.componentInstance;
     });
 
-    xit('should create', () => {
+    it('should create', () => {
         fixture.detectChanges();
         expect(component).toBeTruthy();
+    });
+
+    it('it should render the drawer header', () => {
+        const customFixture = TestBed.createComponent(DrawerHeaderRenderTest);
+        customFixture.detectChanges();
+        expect(customFixture.nativeElement.querySelector('#test-header')).toBeTruthy();
+    });
+
+    it('it should render the drawer subheader', () => {
+        const customFixture = TestBed.createComponent(DrawerSubheaderRenderTest);
+        customFixture.detectChanges();
+        expect(customFixture.nativeElement.querySelector('#test-subheader')).toBeTruthy();
+    });
+
+    it('it should render the drawer body', () => {
+        const customFixture = TestBed.createComponent(DrawerBodyRenderTest);
+        customFixture.detectChanges();
+        expect(customFixture.nativeElement.querySelector('#test-body')).toBeTruthy();
+    });
+
+    it('it should render the drawer footer', () => {
+        const customFixture = TestBed.createComponent(DrawerFooterRenderTest);
+        customFixture.detectChanges();
+        expect(customFixture.nativeElement.querySelector('#test-footer')).toBeTruthy();
+    });
+
+    it('should enforce class naming conventions', () => {
+        fixture.detectChanges();
+        const classList = [
+            '.pxb-drawer',
+        ];
+        for (const className of classList) {
+            count(fixture, className);
+        }
     });
 });
