267cf4b2679aa9bc908a025e25c65d4856af4753
daileytj <daileytj89@gmail.com>
2020-05-22 14:50:45 -0400

    update build tests to include drawer

--- scripts/buildTest.sh
+++ scripts/buildTest.sh
@@ -35,6 +35,11 @@ echo "Checking ScoreCard..."
 echo -ne "  score-card.component: "
 if [ ! -f ./core/score-card/score-card.component.d.ts  ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 
+echo "Checking Drawer..."
+echo -ne "  drawer.component: "
+if [ ! -f ./core/drawer/drawer.component.d.ts  ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+
+
 echo -e "\r\n${GREEN}-----------------------------------"
 echo -e "@pxblue/angular-components package successfully created"
 echo -e "-----------------------------------${NC}\r\n\r\n"
