6e8a94fb29d4cb11113851b8f969babd1edf5497
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-06-05 08:32:45 -0400

    Add spacer to multiple nav group example

--- demos/storybook/stories/drawer/with-multiple-nav-groups.stories.ts
+++ demos/storybook/stories/drawer/with-multiple-nav-groups.stories.ts
@@ -1,5 +1,5 @@
 import { navItems } from './basic-config.stories';
-import { text } from '@storybook/addon-knobs';
+import {boolean, text} from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 export const navItems2 = [
@@ -29,6 +29,7 @@ export const withMultiNavGroups = (): any => ({
                     <mat-icon icon>{{ navItem.icon }}</mat-icon>
                  </pxb-drawer-nav-item>
               </pxb-drawer-nav-group>
+              <pxb-spacer *ngIf="spacer"></pxb-spacer> 
               <pxb-drawer-nav-group [title]="groupTitle2">
                  <pxb-drawer-nav-item *ngFor="let navItem of navItems2"
                     [title]="navItem.title"
@@ -45,5 +46,6 @@ export const withMultiNavGroups = (): any => ({
         navItems2: navItems2,
         groupTitle1: text('NavGroup 1 title', 'Group 1'),
         groupTitle2: text('NavGroup 2 title', 'Group 2'),
+        spacer: boolean('Add Spacer', false)
     },
 });
