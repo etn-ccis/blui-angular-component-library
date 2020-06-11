658105013df2d33a1a75f78d7e14998d855bbcb3
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-06-11 07:20:17 -0400

    Add nested items knobs

--- demos/storybook/stories/drawer/with-nested-nav-items.stories.ts
+++ demos/storybook/stories/drawer/with-nested-nav-items.stories.ts
@@ -1,5 +1,6 @@
 import { action } from '@storybook/addon-actions';
 import { DrawerNavItem } from '@pxblue/angular-components';
+import {boolean} from "@storybook/addon-knobs";
 
 export const nestedNavGroup: DrawerNavItem[] = [
     {
@@ -50,15 +51,21 @@ export const withNestedNavItems = (): any => ({
               <pxb-drawer-nav-group>
                   <pxb-drawer-nav-item *ngFor="let navItem of navItems"
                      [title]="navItem.title"
+                     [divider]="divider"
+                     [hidePadding]="hidePadding"
                      [selected]="state.selected === navItem.title"
                      (select)="navItem.onSelect(); setActive(navItem, state);">
-                     <mat-icon icon>{{ navItem.icon }}</mat-icon>
+                     <mat-icon *ngIf="showIcon" icon>{{ navItem.icon }}</mat-icon>
                      <pxb-drawer-nav-item *ngFor="let nestedItem of navItem.items"
                        [title]="nestedItem.title"
+                       [divider]="dividerNested"
+                       [hidePadding]="hidePaddingNested"
                        [selected]="state.selected === nestedItem.title"
                        (select)="nestedItem.onSelect(); setActive(nestedItem, state);">
                         <pxb-drawer-nav-item *ngFor="let deepItem of nestedItem.items"
                            [title]="deepItem.title"
+                           [divider]="dividerNested"
+                           [hidePadding]="hidePaddingNested"
                            [selected]="state.selected === deepItem.title"
                            (select)="deepItem.onSelect(); setActive(deepItem, state);">
                          </pxb-drawer-nav-item>
@@ -70,6 +77,11 @@ export const withNestedNavItems = (): any => ({
       `,
     props: {
         navItems: nestedNavGroup,
+        showIcon: boolean('Show Icon', true),
+        divider: boolean('divider (top)', false),
+        dividerNested: boolean('divider (nested)', false),
+        hidePadding: boolean('hidePadding (top)', false),
+        hidePaddingNested: boolean('hidePadding (nested)', false),
         setActive: (item: DrawerNavItem, state: { selected: string }): void => {
             if (!item.items) {
                 // Only selects items that do not have nested nav items.
