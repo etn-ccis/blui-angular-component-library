a4f304916b8cb9e8ce73d080e6c868e654cc6adb
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-07-10 12:42:47 -0400

    Void void

--- components/src/core/channel-value/channel-value.component.spec.ts
+++ components/src/core/channel-value/channel-value.component.spec.ts
@@ -48,8 +48,8 @@ describe('ChannelValueComponent', () => {
         fixture.detectChanges();
         const container = fixture.nativeElement.querySelector('.pxb-channel-value');
         const units = fixture.nativeElement.querySelector('.pxb-channel-value-units');
-        void void expect(units.innerHTML).toBe('hz');
-        void void expect(container.children[2]).toBe(units);
+        void expect(units.innerHTML).toBe('hz');
+        void expect(container.children[2]).toBe(units);
     });
 
     it('should render units to the right', () => {
@@ -59,15 +59,15 @@ describe('ChannelValueComponent', () => {
         fixture.detectChanges();
         const container = fixture.nativeElement.querySelector('.pxb-channel-value');
         const units = fixture.nativeElement.querySelector('.pxb-channel-value-units');
-        void void expect(units.innerHTML).toBe('C');
-        void void expect(container.children[1]).toBe(units);
+        void expect(units.innerHTML).toBe('C');
+        void expect(container.children[1]).toBe(units);
     });
 
     it('should render an icon', () => {
         const iconFixture = TestBed.createComponent(TestIcon);
         iconFixture.detectChanges();
         const icon = iconFixture.nativeElement.querySelector('#channel-value-test-icon');
-        void void expect(icon).toBeTruthy();
+        void expect(icon).toBeTruthy();
     });
 
     it('should enforce class naming conventions', () => {
