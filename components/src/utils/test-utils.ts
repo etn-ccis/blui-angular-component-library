a1dda9b87ea6c6e2e27ce39103e1e4909d7c9e22
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-07-13 08:22:11 -0400

    Use length

--- components/src/utils/test-utils.ts
+++ components/src/utils/test-utils.ts
@@ -3,8 +3,8 @@ import { ComponentFixture } from '@angular/core/testing';
 // Using a selector, counts the number of instances.
 export const count = (fixture: ComponentFixture<any>, selector: string, expected = 1): void => {
     const instances = fixture.nativeElement.querySelectorAll(selector);
-    const length: string = instances.length;
-    void expect(instances.length).toBe(
+    const length = instances.length;
+    void expect(length).toBe(
         expected,
         `Expected ${expected} instances of '${selector}', but found ${length}`
     );
