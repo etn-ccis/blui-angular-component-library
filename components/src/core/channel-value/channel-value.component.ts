913bdb3c5b29dc4118f5888ce023d4357857ec6f
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-07-23 10:44:49 -0400

    Make channelValue use bidi

--- components/src/core/channel-value/channel-value.component.ts
+++ components/src/core/channel-value/channel-value.component.ts
@@ -1,12 +1,21 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnChanges,
+    ViewEncapsulation
+} from '@angular/core';
 import { requireInput } from '../../utils/utils';
+import {BidiComponent} from "../utility/bidi.component";
+import {Directionality} from "@angular/cdk/bidi";
 
 @Component({
     selector: 'pxb-channel-value',
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
     template: `
-        <span class="pxb-channel-value" [style.color]="color">
+        <span class="pxb-channel-value" [class.pxb-rtl]="isRtl" [style.color]="color">
             <span class="pxb-channel-value-icon-wrapper">
                 <ng-content></ng-content>
             </span>
@@ -17,11 +26,15 @@ import { requireInput } from '../../utils/utils';
     `,
     styleUrls: ['./channel-value.component.scss'],
 })
-export class ChannelValueComponent implements OnChanges {
+export class ChannelValueComponent extends BidiComponent implements OnChanges {
     @Input() value: string | number;
     @Input() units: string;
     @Input() prefix = false;
     @Input() color: string;
+
+    constructor(dir: Directionality, changeDetectorRef: ChangeDetectorRef) {
+        super(dir, changeDetectorRef);
+    }
 
     ngOnChanges(): void {
         requireInput<ChannelValueComponent>(['value'], this);
