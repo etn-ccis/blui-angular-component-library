a80600eddeff1f472aa27206476da6172914cd3b
emclaug2 <evan.w.mclaughlin@gmail.com>
2021-01-20 16:55:43 -0500

    Match icon button style of react

--- demos/storybook/stories/empty-state/with-actions.stories.ts
+++ demos/storybook/stories/empty-state/with-actions.stories.ts
@@ -6,14 +6,14 @@ export const withActions = (): any => ({
         <pxb-empty-state [title]="title" [description]="description">
             <mat-icon pxb-empty-icon>devices</mat-icon>
             <button pxb-actions mat-stroked-button color="primary" (click)="click()">
-                <mat-icon>add</mat-icon>
+                <mat-icon style="margin-right: 8px; margin-left: -4px;">add</mat-icon>
                 {{actionText}}
             </button>
         </pxb-empty-state>
     `,
     props: {
         title: text('title', 'No Devices'),
-        description: text('description', 'Not a single one'),
+        description: text('description', 'Check your network connection or add a new device'),
         click: action('button clicked'),
         actionText: text('Action Text', 'Add Device'),
     },
