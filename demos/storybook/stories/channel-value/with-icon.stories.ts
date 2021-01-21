caa6e6b924b7c65405a0eb087c39d1477c5af947
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-21 12:51:17 -0500

    RTL channelValue

--- demos/storybook/stories/channel-value/with-icon.stories.ts
+++ demos/storybook/stories/channel-value/with-icon.stories.ts
@@ -1,13 +1,15 @@
 import { color } from '@storybook/addon-knobs';
 import * as Colors from '@pxblue/colors';
+import { invertRTL } from '../../src/utils';
 
 export const withIcon = (): any => ({
     template: `
         <pxb-channel-value [value]="'123'" [units]="'hz'">
-            <mat-icon [style.color]="iconColor">trending_up</mat-icon>
+            <mat-icon [style.color]="iconColor" [style.transform]="invertRTL()">trending_up</mat-icon>
         </pxb-channel-value>
     `,
     props: {
         iconColor: color('icon.color', Colors.red[500]),
+        invertRTL: invertRTL,
     },
 });
