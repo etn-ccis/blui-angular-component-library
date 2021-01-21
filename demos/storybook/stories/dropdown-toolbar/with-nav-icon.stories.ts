897bd9e5886fb8fab2e05d287f7139a76e9d2ab5
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-21 13:35:02 -0500

    RTL styles for DropdonwToolbar

--- demos/storybook/stories/dropdown-toolbar/with-nav-icon.stories.ts
+++ demos/storybook/stories/dropdown-toolbar/with-nav-icon.stories.ts
@@ -1,12 +1,13 @@
 import { text, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import { invertRTL } from '../../src/utils';
 
 export const withNavIcon = (): any => ({
     template: `
        <pxb-dropdown-toolbar [title]="title" [subtitle]="state.selected || subtitle">
             <button mat-icon-button pxb-nav-icon (click)="clickPXBIcon()" aria-label="menu icon">
                 <mat-icon *ngIf="navIcon === 'menu'">menu</mat-icon>
-                <mat-icon *ngIf="navIcon === 'arrow_back'">arrow_back</mat-icon>
+                <mat-icon *ngIf="navIcon === 'arrow_back'" [style.transform]="invertRTL()">arrow_back</mat-icon>
             </button>
         <ng-container pxb-toolbar-menu>
             <button mat-menu-item (click)="updateSubtitle('Test Item 1', state)">Test Item 1</button>
@@ -26,5 +27,6 @@ export const withNavIcon = (): any => ({
             selected: undefined,
         },
         clickPXBIcon: action('pxb nav icon clicked'),
+        invertRTL: invertRTL,
     },
 });
