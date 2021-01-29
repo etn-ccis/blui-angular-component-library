dc6afcb75e339e7bb4de4c73c045c583ce8bc6ac
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-29 14:40:41 -0500

    Fix typo in name, use DeviceDeactivating icon

--- demos/storybook/stories/info-list-item/with-right-content.stories.ts
+++ demos/storybook/stories/info-list-item/with-right-content.stories.ts
@@ -3,7 +3,7 @@ import * as Colors from '@pxblue/colors';
 export const withRightContent = (): any => ({
     template: `
         <pxb-info-list-item>
-            <div pxb-title>Info LIst Item</div>
+            <div pxb-title>Info List Item</div>
             <div pxb-subtitle>with a ChannelValue component to the right</div>
             <mat-icon [style.color]="colors.blue[500]" pxb-icon>battery_charging_full</mat-icon>
             <pxb-channel-value [value]="15" units="A" pxb-right-content></pxb-channel-value>
