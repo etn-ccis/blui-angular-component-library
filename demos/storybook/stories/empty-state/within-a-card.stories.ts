1265ca5daaa094692a8b1f539575cf343637a170
Ekta Ghag <ektasawant99@gmail.com>
2021-10-05 12:06:21 +0530

    fix width, expansion

--- demos/storybook/stories/empty-state/within-a-card.stories.ts
+++ demos/storybook/stories/empty-state/within-a-card.stories.ts
@@ -1,4 +1,3 @@
-import { color, number, text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import * as Colors from '@pxblue/colors';
 import { invertRTL } from '../../src/utils';
@@ -6,21 +5,33 @@ const iconColor = Colors.gray[500];
 export const withinACardConfig = (): any => ({
     template: `
         <mat-accordion>
-            <mat-expansion-panel (opened)="updateState(state, false)" (closed)="updateState(state, true)"  style="width: 394px">
-                <mat-expansion-panel-header>
+            <mat-expansion-panel [expanded]="true" style="width: 392px;">
+                <mat-expansion-panel-header style="border-bottom: 1px solid #d5d8da; border-radius: 0;">
                     <mat-panel-title style="color:#007BC1;">
                         Device Usage
                     </mat-panel-title>
                 </mat-expansion-panel-header>
-                <pxb-empty-state title="No Devices Found" description="After you add devices to this repository, we will show your recent device activities here.">
-                    <mat-icon pxb-empty-icon [style.color]="iconColor" [style.fontSize.px]="100"
-                        [style.transform]="invertRTL()">
-                        help_outline</mat-icon>
+                <pxb-empty-state 
+                    title="No Devices Found"
+                    description="After you add devices to this repository, we will show your recent device activities here."
+                    [style.margin.px]="24"
+                >
+                    <mat-icon pxb-empty-icon
+                        [style.color]="iconColor"
+                        [style.fontSize.px]="100"
+                        [style.transform]="invertRTL()"
+                    >
+                        help_outline
+                    </mat-icon>
                     <button pxb-actions mat-flat-button color="primary" (click)="click()">
-                        <mat-icon style="font-size: 16px;
-                        height: 16px;
-                        width: 15px;
-                        margin-right: 8px;">add</mat-icon>
+                        <mat-icon 
+                            style="font-size: 16px;
+                            height: 16px;
+                            width: 16px;
+                            margin-right: 8px;"
+                        >
+                            add
+                        </mat-icon>
                         Learn More
                     </button>
                 </pxb-empty-state>
@@ -30,11 +41,5 @@ export const withinACardConfig = (): any => ({
     props: {
         click: action('button clicked'),
         invertRTL: invertRTL,
-        updateState: (state, val): void => {
-            state.panelOpenState = val;
-        },
-        state: {
-            panelOpenState: false,
-        },
     },
 });
