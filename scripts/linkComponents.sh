4f162fe7dc558125bd54c8554d9e1657ea852cfb
Evan <evan.w.mclaughlin@gmail.com>
2022-02-11 11:48:43 -0500

    Go NG13 and keep SB at NG12

--- scripts/linkComponents.sh
+++ scripts/linkComponents.sh
@@ -14,18 +14,18 @@ yarn build
 
 echo -en "${BLUE}Creating new folder in node_modules...${NC}"
 rm -rf "./demos/showcase/node_modules/@brightlayer-ui/angular-components"
-rm -rf "./demos/storybook/node_modules/@brightlayer-ui/angular-components"
+# rm -rf "./demos/storybook/node_modules/@brightlayer-ui/angular-components"
 mkdir -p "./demos/showcase/node_modules/@brightlayer-ui/angular-components"
-mkdir -p "./demos/storybook/node_modules/@brightlayer-ui/angular-components"
+# mkdir -p "./demos/storybook/node_modules/@brightlayer-ui/angular-components"
 echo -e "${GREEN}Done${NC}"
 
 echo -en "${BLUE}Copying build output into node_modules...${NC}";
 cp -r ./dist/. ./demos/showcase/node_modules/@brightlayer-ui/angular-components
-cp -r ./dist/. ./demos/storybook/node_modules/@brightlayer-ui/angular-components
+# cp -r ./dist/. ./demos/storybook/node_modules/@brightlayer-ui/angular-components
 echo -e "${GREEN}Done${NC}"
 
 echo -en "\r\n${BBLUE}Linking Components: ${NC}"
 if [ ! -f ./demos/showcase/node_modules/@brightlayer-ui/angular-components/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
-if [ ! -f ./demos/storybook/node_modules/@brightlayer-ui/angular-components/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
+# if [ ! -f ./demos/storybook/node_modules/@brightlayer-ui/angular-components/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
 
 echo -e "${GRAY}Complete${NC}\r\n"
