e14666013def87afed9c8f4c431c12c12c001acb
Evan <evan.w.mclaughlin@gmail.com>
2022-07-28 12:09:56 -0400

    Make the example a little less tall

--- src/docs/pages/component-docs/components/drawer/drawer-nav-group/examples/with-spacer.component.ts
+++ src/docs/pages/component-docs/components/drawer/drawer-nav-group/examples/with-spacer.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { DrawerComponent } from '@brightlayer-ui/angular-components';
 
-export const SPACER = `<blui-drawer style="width: 250px; height: 400px">
+export const SPACER = `<blui-drawer style="width: 250px; height: 350px">
     <blui-drawer-body>
         <blui-drawer-nav-group title="Group 1">
             <blui-drawer-nav-item title="Item 1"></blui-drawer-nav-item>
