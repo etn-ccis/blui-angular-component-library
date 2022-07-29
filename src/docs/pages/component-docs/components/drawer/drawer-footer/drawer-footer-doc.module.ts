ff7175ba8d070e9044aa6a613c0c2dc2520a34c6
Evan <evan.w.mclaughlin@gmail.com>
2022-07-29 09:49:18 -0400

    Run prettier

--- src/docs/pages/component-docs/components/drawer/drawer-footer/drawer-footer-doc.module.ts
+++ src/docs/pages/component-docs/components/drawer/drawer-footer/drawer-footer-doc.module.ts
@@ -3,7 +3,7 @@ import { DrawerModule } from '@brightlayer-ui/angular-components';
 import { SharedCompDocsModule } from '../../../shared/shared-comp-docs.module';
 import { DrawerFooterDocComponent } from './drawer-footer-doc.component';
 import { BasicExampleComponent } from './examples/basic.component';
-import {ComplexExampleComponent} from "./examples/complex.component";
+import { ComplexExampleComponent } from './examples/complex.component';
 
 @NgModule({
     declarations: [DrawerFooterDocComponent, BasicExampleComponent, ComplexExampleComponent],
