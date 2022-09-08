9d93f3466f3d761c79b98dbe1947943cd80960d9
Evan <evan.w.mclaughlin@gmail.com>
2022-09-08 07:56:02 -0400

    Code review changes

--- src/docs/pages/component-docs/components/hero/examples/icon-diffs.component.ts
+++ src/docs/pages/component-docs/components/hero/examples/icon-diffs.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
-import * as Colors from '@brightlayer-ui/colors';
 
 export const ICON_DIFFS = `<blui-hero-banner *ngFor="let size of [36, 48, 72]">
-    <blui-hero label="SVG" [value]="size + 'px'" 
-        [iconBackgroundColor]="bluiWhite" [iconSize]="size">
-        <mat-icon blui-primary svgIcon="blui-icons:voltage_circled_outline" 
-            [style.color]="bluiBlue">
+    <blui-hero label="SVG" [value]="size + 'px'" [iconSize]="size">
+        <mat-icon blui-primary svgIcon="blui-icons:voltage_circled_outline">
         </mat-icon>
     </blui-hero>
-    <blui-hero label="mat icon" [value]="size + 'px'" 
-        [iconBackgroundColor]="bluiWhite" [iconSize]="size">
+    <blui-hero label="mat icon" [value]="size + 'px'" [iconSize]="size">
         <mat-icon blui-primary>schedule</mat-icon>
     </blui-hero>
-    <blui-hero label="web icon" [value]="size + 'px'" 
-        [iconBackgroundColor]="bluiWhite" [iconSize]="size">
-        <i blui-primary [style.color]="bluiGreen" class="blui-current_circled"></i>
+    <blui-hero label="icon font" [value]="size + 'px'"  [iconSize]="size">
+        <i blui-primary class="blui-current_circled"></i>
     </blui-hero>
-    <blui-hero label="PNG" [value]="size + 'px'" 
-        [iconBackgroundColor]="bluiWhite" [iconSize]="size">
+    <blui-hero label="PNG" [value]="size + 'px'" [iconSize]="size">
         <img blui-primary src="assets/trex.png" alt="A T-Rex as the avatar image" />
     </blui-hero>
 </blui-hero-banner>
@@ -27,9 +21,4 @@ export const ICON_DIFFS = `<blui-hero-banner *ngFor="let size of [36, 48, 72]">
     selector: 'app-icon-diffs-hero-demo',
     template: ICON_DIFFS,
 })
-export class IconDiffsComponent {
-    bluiBlue = Colors.blue[500];
-    bluiWhite = Colors.white[50];
-    bluiRed = Colors.red[500];
-    bluiGreen = Colors.green[500];
-}
+export class IconDiffsComponent {}
