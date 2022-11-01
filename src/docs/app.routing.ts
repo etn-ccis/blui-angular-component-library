9ec7f3ece01b937a7a4dd4a7136b284db73d577a
Evan <evan.w.mclaughlin@gmail.com>
2022-11-01 12:49:32 -0400

    Address situation where tab is omitted from route

--- src/docs/app.routing.ts
+++ src/docs/app.routing.ts
@@ -24,26 +24,88 @@ import { ToolbarMenuDocComponent } from './pages/component-docs/components/toolb
 
 const routes: Routes = [
     { path: '', redirectTo: `${COMPONENT_NAV_ITEMS.appBar.route}/examples`, pathMatch: 'full' },
+
+    { path: `${COMPONENT_NAV_ITEMS.appBar.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.appBar.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.appBar.route}/:tab`, component: AppBarDocComponent },
+
+    {
+        path: `${COMPONENT_NAV_ITEMS.channelValue.route}`,
+        redirectTo: `${COMPONENT_NAV_ITEMS.channelValue.route}/examples`,
+    },
     { path: `${COMPONENT_NAV_ITEMS.channelValue.route}/:tab`, component: ChannelValueDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.hero.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.hero.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.hero.route}/:tab`, component: HeroDocComponent },
+
+    {
+        path: `${COMPONENT_NAV_ITEMS.listItemTag.route}`,
+        redirectTo: `${COMPONENT_NAV_ITEMS.listItemTag.route}/examples`,
+    },
     { path: `${COMPONENT_NAV_ITEMS.listItemTag.route}/:tab`, component: ListItemTagDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerLayout.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerLayout.route}/examples` },
     { path: `${DRAWER_NAV_ITEMS.drawerLayout.route}/:tab`, component: DrawerLayoutDocComponent },
-    { path: `${DRAWER_NAV_ITEMS.drawerDrawer.route}/:tab`, component: DrawerDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerHeader.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerHeader.route}/examples` },
     { path: `${DRAWER_NAV_ITEMS.drawerHeader.route}/:tab`, component: DrawerHeaderDocComponent },
+
+    {
+        path: `${DRAWER_NAV_ITEMS.drawerSubheader.route}`,
+        redirectTo: `${DRAWER_NAV_ITEMS.drawerSubheader.route}/examples`,
+    },
     { path: `${DRAWER_NAV_ITEMS.drawerSubheader.route}/:tab`, component: DrawerSubheaderDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerBody.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerBody.route}/examples` },
     { path: `${DRAWER_NAV_ITEMS.drawerBody.route}/:tab`, component: DrawerBodyDocComponent },
+
+    {
+        path: `${DRAWER_NAV_ITEMS.drawerNavGroup.route}`,
+        redirectTo: `${DRAWER_NAV_ITEMS.drawerNavGroup.route}/examples`,
+    },
     { path: `${DRAWER_NAV_ITEMS.drawerNavGroup.route}/:tab`, component: DrawerNavGroupDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerNavItem.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerNavItem.route}/examples` },
     { path: `${DRAWER_NAV_ITEMS.drawerNavItem.route}/:tab`, component: DrawerNavItemDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerFooter.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerFooter.route}/examples` },
     { path: `${DRAWER_NAV_ITEMS.drawerFooter.route}/:tab`, component: DrawerFooterDocComponent },
+
+    { path: `${DRAWER_NAV_ITEMS.drawerDrawer.route}`, redirectTo: `${DRAWER_NAV_ITEMS.drawerDrawer.route}/examples` },
+    { path: `${DRAWER_NAV_ITEMS.drawerDrawer.route}/:tab`, component: DrawerDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.emptyState.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.emptyState.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.emptyState.route}/:tab`, component: EmptyStateDocComponent },
+
+    {
+        path: `${COMPONENT_NAV_ITEMS.infoListItem.route}`,
+        redirectTo: `${COMPONENT_NAV_ITEMS.infoListItem.route}/examples`,
+    },
     { path: `${COMPONENT_NAV_ITEMS.infoListItem.route}/:tab`, component: InfoListItemDocComponent },
+
+    {
+        path: `${COMPONENT_NAV_ITEMS.mobileStepper.route}`,
+        redirectTo: `${COMPONENT_NAV_ITEMS.mobileStepper.route}/examples`,
+    },
     { path: `${COMPONENT_NAV_ITEMS.mobileStepper.route}/:tab`, component: MobileStepperDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.threeLiner.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.threeLiner.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.threeLiner.route}/:tab`, component: ThreeLinerDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.scoreCard.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.scoreCard.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.scoreCard.route}/:tab`, component: ScoreCardDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.spacer.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.spacer.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.spacer.route}/:tab`, component: SpacerDocComponent },
+
+    {
+        path: `${COMPONENT_NAV_ITEMS.toolbarMenu.route}`,
+        redirectTo: `${COMPONENT_NAV_ITEMS.toolbarMenu.route}/examples`,
+    },
     { path: `${COMPONENT_NAV_ITEMS.toolbarMenu.route}/:tab`, component: ToolbarMenuDocComponent },
+
+    { path: `${COMPONENT_NAV_ITEMS.userMenu.route}`, redirectTo: `${COMPONENT_NAV_ITEMS.userMenu.route}/examples` },
     { path: `${COMPONENT_NAV_ITEMS.userMenu.route}/:tab`, component: UserMenuDocComponent },
+    { path: '**', redirectTo: `${COMPONENT_NAV_ITEMS.appBar.route}/examples`, pathMatch: 'full' },
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
