8826d51c0e662b121b68ed8b24949772bc59f7fb
RaghavendranC-Eaton <RaghavendranC@eaton.com>
2023-07-08 01:33:18 +0530

    added showcase folder as git submodule

--- showcase/src/app/pages/mat/navigation/navigation.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-    selector: 'app-mat-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-})
-export class MatNavigationComponent {}
